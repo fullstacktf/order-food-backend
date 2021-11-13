491dacad15e8b2feb4449c17ec67312ba2258843
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-13 01:11:27 +0000

    built simple server using gin-gonic

--- src/app.go
+++ src/app.go
@@ -1,1 +1,22 @@
 package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"log"
+)
+
+func main() {
+	r := gin.Default()
+
+	r.GET("/", getHandler)
+
+	if err := r.Run(":3000"); err != nil {
+		log.Fatal(err.Error())
+	}
+}
+
+func getHandler(c *gin.Context) {
+	c.JSON(200, gin.H{
+		"message": "You are at comiditapp homepage🥳🥳",
+	})
+}
