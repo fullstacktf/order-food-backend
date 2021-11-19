effee29f901b199614dbc8a3ac1fdee8e464baf5
Nicolás Cabrera <jncabdom@gmail.com>
2021-11-19 16:24:14 +0000

    Corrected volume route for mongodb container

--- .scripts/build_mongo.sh
+++ .scripts/build_mongo.sh
@@ -2,4 +2,4 @@
 
 # Runs the docker container on port 27017:27017 with name "mongo" on dettached mode
 # more details: when stopped it's deleted and it has a volume on ".cache" directory
-docker run --rm -d --name mongo -p 27017:27017 -v $(pwd)/.cache:/data/db mongo:5.0.3 
+docker run --rm -d --name mongo -p 27017:27017 -v $(pwd)/.cache/db/data:/data/db mongo:5.0.3 
