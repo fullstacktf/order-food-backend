bace4c73dffff3edca156523f0d780c55ff83e55
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-21 18:28:40 +0100

    chore: Adds some enums to handle types in our models, and adds changes to the models

---
+++ src/enums/product_categories.go
@@ -0,0 +1,11 @@
+package enums
+
+type ProductCategory string
+
+const (
+	Appetizer  ProductCategory = "appetizer"
+	MainCourse ProductCategory = "mainCourse"
+	Dessert    ProductCategory = "dessert"
+	Drink      ProductCategory = "drink"
+	Other      ProductCategory = "other"
+)
