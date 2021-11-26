bb487b91facd59bc9e1b9c01590f3269ef177a1e
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-26 01:57:04 +0100

    WIP: added handler for getting order from user profile orders by id (not working)

--- src/handlers/any_role/profile/get_order_by_id.go
+++ src/handlers/any_role/profile/get_order_by_id.go
@@ -1,11 +1,36 @@
 package profile_handler
 
 import (
+	"comiditapp/api/database"
+	"comiditapp/api/env"
+	"comiditapp/api/models"
+	"context"
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 func GetOrderById(c *gin.Context) {
-	c.String(http.StatusOK, "GetOrderById handler")
+	db := database.GetDB()
+	col := db.Client.Database(env.DB_NAME).Collection("orders")
+
+	orderId := c.Param("id")
+	orderObjectID, err := primitive.ObjectIDFromHex(orderId)
+
+	if err != nil {
+		panic(err)
+	}
+
+	var result models.Order
+	findErr := col.FindOne(context.TODO(), bson.M{"id": orderObjectID}).Decode(&result)
+
+	if findErr != nil {
+		fmt.Println("Error calling FindOne() on GetOrderById:", findErr)
+		panic(findErr)
+	}
+
+	c.JSON(http.StatusOK, result)
 }
