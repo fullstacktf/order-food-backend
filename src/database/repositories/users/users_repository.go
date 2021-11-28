63844fd63e24b0d1bb7c700f306b1fc2430ea2d3
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-28 16:53:43 +0100

    repository: Updates users_repository methods

--- src/database/repositories/users/users_repository.go
+++ src/database/repositories/users/users_repository.go
@@ -1,14 +1,18 @@
 package repository
 
-import (
-	"comiditapp/api/models"
-	"context"
-
-	"go.mongodb.org/mongo-driver/bson/primitive"
-)
+import "github.com/gin-gonic/gin"
 
 type UsersRepository interface {
-	createUser(ctx context.Context, user models.User) // Endpoint -> /auth/signup
-	updateUser(ctx context.Context, user models.User) // Endpoint -> /profile
-	findUserById(ctx context.Context, id primitive.ObjectID)
+	// any_role methods
+	SignUpUser(context *gin.Context)            // Endpoint -> /auth/signup
+	SignInUser(context *gin.Context)            // Endpoint -> /auth/signin
+	FindRestaurants(context *gin.Context)       // Endpoint -> /restaurants
+	GetRestaurantById(context *gin.Context)     // Endpoint -> /restaurants/:id
+	GetRestaurantProducts(context *gin.Context) // Endpoint -> /restaurants/:id/products
+	UpdateProfile(context *gin.Context)         // Endpoint -> /profile
+
+	// restaurant_role methods
+	FindProducts(context *gin.Context)  // Endpoint -> /products
+	CreateProduct(context *gin.Context) // Endpoint -> /products
+	UpdateProduct(context *gin.Context) // Endpoint -> /products/:id
 }
