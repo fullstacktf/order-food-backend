52300df7c7899eabf16f2aaef943ee8d54f7087c
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-28 19:20:52 +0100

    repository: Adds almost all endpoints implementation for mocked users repository

--- src/database/repositories/users/mocked_users_repository.go
+++ src/database/repositories/users/mocked_users_repository.go
@@ -1,19 +1,29 @@
 package repository
 
 import (
+	"comiditapp/api/enums"
 	"comiditapp/api/models"
+	"encoding/json"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
 	"github.com/stretchr/testify/mock"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-var orders []models.User = []models.User{
-	{Id: primitive.NewObjectID(), Role: "client", Name: "Francisco Díaz", Email: "franDiaz@yahoo.es", HashedPassword: "MichaelHighFives", Phone: 111111111, Address: []models.Address{{Street: "Calle Roquefort 32", ZipCode: 11111, Country: "España", City: "Las Palmas de Gran Canaria"}}},
-	{Id: primitive.NewObjectID(), Role: "client", Name: "María Fernández", Email: "mery-mail@hotmail.com", HashedPassword: "BrooklynDisguises", Phone: 222222222, Address: []models.Address{{Street: "Calle Benito 41", ZipCode: 22222, Country: "España", City: "Córdoba"}}},
-	{Id: primitive.NewObjectID(), Role: "restaurant", Name: "Cafetería Mortadelo", Email: "cafeteriamortadelo@hotmail.es", HashedPassword: "HappyChickens", Phone: 111111111, Address: []models.Address{{Street: "Avenida de los Majuelos 117", ZipCode: 11111, Country: "España", City: "Santa Cruz de Tenerife"}},
+// This is just to ensure that the always have the same id's and dont generate new one after a server reload
+var id1 = primitive.NewObjectID()
+var id2 = primitive.NewObjectID()
+var id3 = primitive.NewObjectID()
+var id4 = primitive.NewObjectID()
+
+var users []models.User = []models.User{
+	{Id: id1, Role: enums.Client, Name: "Francisco Díaz", Email: "franDiaz@yahoo.es", HashedPassword: "MichaelHighFives", Phone: 111111111, Address: []models.Address{{Street: "Calle Roquefort 32", ZipCode: 11111, Country: "España", City: "Las Palmas de Gran Canaria"}}},
+	{Id: id2, Role: enums.Client, Name: "María Fernández", Email: "mery-mail@hotmail.com", HashedPassword: "BrooklynDisguises", Phone: 222222222, Address: []models.Address{{Street: "Calle Benito 41", ZipCode: 22222, Country: "España", City: "Córdoba"}}},
+	{Id: id3, Role: enums.Restaurant, Name: "Cafetería Mortadelo", Email: "cafeteriamortadelo@hotmail.es", HashedPassword: "HappyChickens", Phone: 111111111, Address: []models.Address{{Street: "Avenida de los Majuelos 117", ZipCode: 11111, Country: "España", City: "Santa Cruz de Tenerife"}},
 		Menu: []models.Product{{Id: "1", Category: "mainCourse", Name: "Bocadillo Clásico Embutidos", Price: 1.40}, {Id: "2", Category: "mainCourse", Name: "Perrito Especial Mexicano", Price: 3.00}, {Id: "3", Category: "appetizer", Name: "Ensaladilla", Price: 4.20}}},
-	{Id: primitive.NewObjectID(), Role: "restaurant", Name: "Don Perrito", Email: "DonPerrito@hotmail.com", HashedPassword: "r,<C:RW(x+2xW}{~", Phone: 222222222, Address: []models.Address{{Street: "Calle Decano Consular Jesus Ramos González 18", ZipCode: 22222, Country: "España", City: "Santa Cruz de Tenerife"}},
+	{Id: id4, Role: enums.Restaurant, Name: "Don Perrito", Email: "DonPerrito@hotmail.com", HashedPassword: "r,<C:RW(x+2xW}{~", Phone: 222222222, Address: []models.Address{{Street: "Calle Decano Consular Jesus Ramos González 18", ZipCode: 22222, Country: "España", City: "Santa Cruz de Tenerife"}},
 		Menu: []models.Product{{Id: "1", Category: "mainCourse", Name: "Ensalada Don Perrito", Price: 4.00}, {Id: "2", Category: "mainCourse", Name: "Ensalada Vegetal", Price: 4.20}, {Id: "3", Category: "drink", Name: "Cerveza Lata", Price: 1.60}}},
 }
 
@@ -24,11 +34,28 @@ type MockedUsersRepository struct {
 // any role
 func (r *MockedUsersRepository) SignUpUser(context *gin.Context) models.User {
 
+	var validate *validator.Validate = validator.New()
+	var newUser models.User
+
+	json.NewDecoder(context.Request.Body).Decode(&newUser)
+
+	err := validate.Struct(newUser)
+	if err != nil {
+		errorMsg := "Cannot create order, required fields not provided...\n" + err.(validator.ValidationErrors).Error()
+
+		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
+		panic(err)
+	}
+
+	users = append(users, newUser)
+	context.IndentedJSON(http.StatusOK, newUser)
+
 	args := r.Called(context)
 	return args.Get(0).(models.User)
 }
 
 func (r *MockedUsersRepository) SignInUser(context *gin.Context) models.User {
+	// needs session info
 
 	args := r.Called(context)
 	return args.Get(0).(models.User)
@@ -36,23 +63,172 @@ func (r *MockedUsersRepository) SignInUser(context *gin.Context) models.User {
 
 func (r *MockedUsersRepository) UpdateProfile(context *gin.Context) models.User {
 
+	var validate *validator.Validate = validator.New()
+	var newUserProfile models.User
+
+	userId, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMsg := "Bad Request, " + err.Error()
+
+		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
+		panic(err)
+	}
+
+	foundIndex := -1
+	for index, value := range users {
+		if value.Id == userId {
+			foundIndex = index
+		}
+	}
+
+	if foundIndex == -1 {
+		errorMsg := "Cannot found user with ID " + userId.Hex()
+		http.Error(context.Writer, errorMsg, http.StatusNotFound)
+		panic(errorMsg)
+	}
+
+	json.NewDecoder(context.Request.Body).Decode(&newUserProfile)
+
+	valErr := validate.Struct(newUserProfile)
+	if valErr != nil {
+		errorMsg := "Cannot update user, required fields not provided...\n" + valErr.(validator.ValidationErrors).Error()
+
+		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
+		panic(valErr)
+	}
+
+	users[foundIndex] = newUserProfile
+
+	context.IndentedJSON(http.StatusOK, newUserProfile)
+
 	args := r.Called(context)
 	return args.Get(0).(models.User)
 }
 
-func (r *MockedUsersRepository) FindRestaurants(context *gin.Context) models.User {
+func (r *MockedUsersRepository) FindRestaurants(context *gin.Context) []models.User {
+
+	result := []models.User{}
+
+	for _, val := range users {
+		if val.Role == enums.Restaurant {
+			result = append(result, val)
+		}
+	}
+
+	context.IndentedJSON(http.StatusOK, result)
 
 	args := r.Called(context)
-	return args.Get(0).(models.User)
+	return args.Get(0).([]models.User)
+}
+
+func (r *MockedUsersRepository) FindClients(context *gin.Context) []models.User {
+
+	result := []models.User{}
+
+	for _, val := range users {
+		if val.Role == enums.Client {
+			result = append(result, val)
+		}
+	}
+
+	context.IndentedJSON(http.StatusOK, result)
+
+	args := r.Called(context)
+	return args.Get(0).([]models.User)
 }
 
 func (r *MockedUsersRepository) GetRestaurantById(context *gin.Context) models.User {
+
+	result := models.User{}
+	restaurantId, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMsg := "Bad Request, " + err.Error()
+
+		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
+		panic(err)
+	}
+
+	for _, val := range users {
+		if val.Id == restaurantId {
+			if val.Role == enums.Restaurant {
+				result = val
+				break
+			} else {
+				errorMsg := "Cannot found restaurant with ID " + restaurantId.String()
+				http.Error(context.Writer, errorMsg, http.StatusNotFound)
+				panic(errorMsg)
+			}
+		}
+	}
+
+	context.IndentedJSON(http.StatusOK, result)
+
+	args := r.Called(context)
+	return args.Get(0).(models.User)
+}
+
+func (r *MockedUsersRepository) GetClientById(context *gin.Context) models.User {
+
+	result := models.User{}
+	clientId, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMsg := "Bad Request, " + err.Error()
+
+		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
+		panic(err)
+	}
+
+	for _, val := range users {
+		if val.Id == clientId {
+			if val.Role == enums.Client {
+				result = val
+				break
+			} else {
+				errorMsg := "Cannot found client with ID " + clientId.String()
+				http.Error(context.Writer, errorMsg, http.StatusNotFound)
+				panic(errorMsg)
+			}
+		}
+	}
+
+	context.IndentedJSON(http.StatusOK, result)
 
 	args := r.Called(context)
 	return args.Get(0).(models.User)
 }
 
 func (r *MockedUsersRepository) GetRestaurantProducts(context *gin.Context) []models.Product {
+
+	result := []models.Product{}
+
+	restaurantId, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMsg := "Bad Request, " + err.Error()
+
+		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
+		panic(err)
+	}
+
+	for _, val := range users {
+		if val.Id == restaurantId {
+			if val.Role == enums.Restaurant {
+				result = append(result, val.Menu...)
+				break
+			} else {
+				errorMsg := "Cannot found restaurant with ID " + restaurantId.String()
+				http.Error(context.Writer, errorMsg, http.StatusNotFound)
+				panic(errorMsg)
+			}
+		}
+	}
+
+	if len(result) == 0 {
+		errorMsg := "Cannot found products for restaurant with ID " + restaurantId.String()
+		http.Error(context.Writer, errorMsg, http.StatusNotFound)
+		panic(errorMsg)
+	}
+
+	context.IndentedJSON(http.StatusOK, result)
 
 	args := r.Called(context)
 	return args.Get(0).([]models.Product)
@@ -61,18 +237,21 @@ func (r *MockedUsersRepository) GetRestaurantProducts(context *gin.Context) []mo
 // restaurant role
 
 func (r *MockedUsersRepository) FindProducts(context *gin.Context) models.User {
+	// needs session info
 
 	args := r.Called(context)
 	return args.Get(0).(models.User)
 }
 
 func (r *MockedUsersRepository) CreateProduct(context *gin.Context) models.User {
+	// needs session info
 
 	args := r.Called(context)
 	return args.Get(0).(models.User)
 }
 
 func (r *MockedUsersRepository) UpdateProduct(context *gin.Context) models.User {
+	// needs session info
 
 	args := r.Called(context)
 	return args.Get(0).(models.User)
