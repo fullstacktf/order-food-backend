b2bd70e51f0ee5ef2ab28c74d15baea16045e63c
Nestor Torres <alu0101021768@ull.edu.es>
2021-11-29 01:04:20 +0100

    changes: Added repositories and database fields to the DB struct we already have.

--- src/database/db.go
+++ src/database/db.go
@@ -1,8 +1,16 @@
 package database
 
-import "go.mongodb.org/mongo-driver/mongo"
+import (
+	orders_repository "comiditapp/api/database/repositories/orders"
+	users_repository "comiditapp/api/database/repositories/users"
+
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type DB struct {
-	Client      *mongo.Client
-	Collections map[string]*mongo.Collection
+	Client           *mongo.Client
+	Database         *mongo.Database
+	Collections      map[string]*mongo.Collection
+	OrdersRepository orders_repository.MongoOrdersRepository
+	UsersRepository  users_repository.MongoUsersRepository
 }
