68db2bdc2eb88f61cf14643d745fe107ecae988c
Nestor Torres <alu0101021768@ull.edu.es>
2021-12-05 01:37:07 +0000

    repositories: adds use cases implementations and error handling. Also adds some explanations of how these handlers should work and how they working rn

--- src/database/repositories/orders/mongo_orders_repository.go
+++ src/database/repositories/orders/mongo_orders_repository.go
@@ -2,34 +2,160 @@ package repository
 
 import (
 	"comiditapp/api/models"
+	"encoding/json"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type MongoOrdersRepository struct {
-	db *mongo.Database
+	orders *mongo.Collection
 }
 
 func NewMongoOrdersRepository(db *mongo.Database) *MongoOrdersRepository {
-	return &MongoOrdersRepository{db: db}
+	return &MongoOrdersRepository{orders: db.Collection("orders")}
 }
 
 // any_role methods
-func (r *MongoOrdersRepository) FindOrders(context *gin.Context) *[]models.Order {
-	return &[]models.Order{}
+
+// GET - http://localhost:3000/profile/orders
+// Actualmente devuelve todas las orders.
+// Debería usar información de la sesión para devolver solo las del user concreto.
+// Otra opcion es que se pidan todas, y luego en front se filtren las del usuario.
+func (r *MongoOrdersRepository) FindOrders(context *gin.Context) (statusCode int, response interface{}) {
+	foundOrders, err := r.orders.Find(context, bson.M{})
+	if err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	orders := []*models.Order{}
+	if err := foundOrders.All(context, &orders); err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	return http.StatusOK, orders
 }
-func (r *MongoOrdersRepository) GetOrderById(context *gin.Context) *models.Order {
-	return &models.Order{}
+
+// GET - http://localhost:3000/profile/orders/:id
+// Devuelve bien la order concreta.
+// Debería usar información de la sesión para devolver la order solo si pertenece al user.
+// Otra opcion es que se pida, y que luego en front no se le dé al user si no es de el.
+func (r *MongoOrdersRepository) GetOrderById(context *gin.Context) (statusCode int, response interface{}) {
+	id, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMessage := "Bad request, " + context.Param("id") + " is not a valid ID"
+		return http.StatusBadRequest, errorMessage
+	}
+
+	filter := bson.M{"id": id}
+	foundOrders, err := r.orders.Find(context, filter)
+	if err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	orders := []*models.Order{}
+	if err := foundOrders.All(context, &orders); err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	return http.StatusOK, orders
 }
-func (r *MongoOrdersRepository) CreateOrder(context *gin.Context) *models.Order {
-	return &models.Order{}
+
+// Para que un usuario cree una order, deberia existir el mismo, deberia existir el restaurante sobre el que va
+// a efectuar la order. La info del user que hace la order la sacariamos de la sesion
+// POST - http://localhost:3000/restaurants/:id/order
+// Actualmente crea una order, pero esta debería sacar el id del cliente de la sesion
+// y el id del restaurante no es requerido porque lo sacamos de :id
+func (r *MongoOrdersRepository) CreateOrder(context *gin.Context) (statusCode int, response interface{}) {
+	var validate *validator.Validate = validator.New()
+	var newOrder models.Order
+
+	json.NewDecoder(context.Request.Body).Decode(&newOrder)
+
+	err := validate.Struct(newOrder)
+	if err != nil {
+		validatorError := err.(validator.ValidationErrors).Error()
+		errorMessage := "Cannot create order, required fields not provided\n" + validatorError
+		return http.StatusBadRequest, errorMessage
+	}
+
+	restaurantId, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMessage := "Bad request, " + context.Param("id") + " is not a valid ID"
+		return http.StatusBadRequest, errorMessage
+	}
+
+	newId := primitive.NewObjectID()
+	order := &models.Order{
+		Id:           newId,
+		ClientId:     newOrder.ClientId,
+		RestaurantId: restaurantId,
+		Status:       newOrder.Status,
+		TotalPrice:   newOrder.TotalPrice,
+		Products:     newOrder.Products,
+	}
+
+	if _, err := r.orders.InsertOne(context, order); err != nil {
+		return http.StatusBadRequest, err.Error()
+	}
+
+	message := "Order " + newId.Hex() + " created succesfully"
+	return http.StatusCreated, message
 }
 
 // restaurant_role methods
-func (r *MongoOrdersRepository) UpdateClientOrder(context *gin.Context) *models.Order {
-	return &models.Order{}
+
+// Para esto necesitamos los permisos del restaurante para realizar las acciones, se sacará del JWT
+// PUT - http://localhost:3000/orders/:id
+// Actualmente actualiza bien la order, pero esta acción debería estar limitada al restaurante encargado
+// de dicha order.
+// Otro aspecto a tener en cuenta, es que cuando se implemente el JWT, ninguno de los ID de cliente, order, o restaurante
+// deberian ser obligatorios, de manera que el usuario solo actualizará el resto de campos, ya que los de id no le incumben
+func (r *MongoOrdersRepository) UpdateClientOrder(context *gin.Context) (statusCode int, response interface{}) {
+	var validate *validator.Validate = validator.New()
+	var newOrder models.Order
+
+	json.NewDecoder(context.Request.Body).Decode(&newOrder)
+
+	err := validate.Struct(newOrder)
+	if err != nil {
+		validatorError := err.(validator.ValidationErrors).Error()
+		errorMessage := "Cannot update order, required fields not provided\n" + validatorError
+		return http.StatusBadRequest, errorMessage
+	}
+
+	orderId, err := primitive.ObjectIDFromHex(context.Param("id"))
+	if err != nil {
+		errorMessage := "Bad request, " + context.Param("id") + " is not a valid ID"
+		return http.StatusBadRequest, errorMessage
+	}
+
+	order := &models.Order{
+		Id:           orderId,
+		RestaurantId: newOrder.RestaurantId,
+		ClientId:     newOrder.ClientId,
+		Status:       newOrder.Status,
+		TotalPrice:   newOrder.TotalPrice,
+		Products:     newOrder.Products,
+	}
+
+	if _, err := r.orders.InsertOne(context, order); err != nil {
+		return http.StatusBadRequest, err.Error()
+	}
+
+	message := "Order " + orderId.Hex() + " updated succesfully"
+	return http.StatusOK, message
 }
-func (r *MongoOrdersRepository) FindClientOrders(context *gin.Context) *[]models.Order {
-	return &[]models.Order{}
+
+// Para esto necesitamos los permisos del restaurante para realizar las acciones, se sacará del JWT
+// GET - http://localhost:3000/orders
+// Este endpoint es como el de devolver todas las orders, solo que cuando tengamos JWT sabremos el restaurante
+// cuyas orders hay que traer. No lo hago porque es casi idéntico, esperaré a tener JWT
+func (r *MongoOrdersRepository) FindClientOrders(context *gin.Context) (statusCode int, response interface{}) {
+
+	return 0, &[]models.Order{}
 }
--- src/database/repositories/users/mongo_users_repository.go
+++ src/database/repositories/users/mongo_users_repository.go
@@ -7,6 +7,7 @@ import (
 
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
@@ -20,7 +21,8 @@ func NewMongoUsersRepository(db *mongo.Database) *MongoUsersRepository {
 }
 
 // any_role methods
-func (r *MongoUsersRepository) SignUpUser(context *gin.Context) *mongo.InsertOneResult {
+// A este método probablemente sea al que tengamos que añadir cosas del JWT
+func (r *MongoUsersRepository) SignUpUser(context *gin.Context) (statusCode int, response interface{}) {
 
 	var validate *validator.Validate = validator.New()
 	var newUser models.User
@@ -28,16 +30,15 @@ func (r *MongoUsersRepository) SignUpUser(context *gin.Context) *mongo.InsertOne
 	json.NewDecoder(context.Request.Body).Decode(&newUser)
 
 	err := validate.Struct(newUser)
-
 	if err != nil {
-		errorMsg := "Cannot create user, required fields not provided\n" + err.(validator.ValidationErrors).Error()
-
-		http.Error(context.Writer, errorMsg, http.StatusBadRequest)
-		panic(errorMsg)
+		validatorError := err.(validator.ValidationErrors).Error()
+		errorMessage := "Cannot create user, required fields not provided\n" + validatorError
+		return http.StatusBadRequest, errorMessage
 	}
 
+	newId := primitive.NewObjectID()
 	user := &models.User{
-		Id:             primitive.NewObjectID(),
+		Id:             newId,
 		Role:           newUser.Role,
 		Name:           newUser.Name,
 		Email:          newUser.Email,
@@ -47,47 +48,88 @@ func (r *MongoUsersRepository) SignUpUser(context *gin.Context) *mongo.InsertOne
 		Menu:           newUser.Menu,
 	}
 
-	createdUserId, err := r.users.InsertOne(context, user)
-	if err != nil {
-		http.Error(context.Writer, err.Error(), http.StatusConflict)
-		panic(err)
+	if _, err := r.users.InsertOne(context, user); err != nil {
+		return http.StatusBadRequest, err.Error()
 	}
 
-	// No se si deberiamos usar el context aqui dentro o delegarlo al handler
-	// context.IndentedJSON(http.StatusCreated, createdUserId)
-
-	return createdUserId
+	message := "User " + newId.Hex() + " updated succesfully"
+	return http.StatusCreated, message
 }
 
-func (r *MongoUsersRepository) SignInUser(context *gin.Context) *models.User {
-	return &models.User{}
+// TODO: Implement JWT auth
+func (r *MongoUsersRepository) SignInUser(context *gin.Context) (statusCode int, response interface{}) {
+	return 0, &models.User{}
 }
-func (r *MongoUsersRepository) FindRestaurants(context *gin.Context) *[]models.User {
-	return &[]models.User{}
+func (r *MongoUsersRepository) FindRestaurants(context *gin.Context) (statusCode int, response interface{}) {
+
+	foundRestaurants, err := r.users.Find(context, bson.M{"role": "restaurant"})
+	if err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	restaurants := []*models.User{}
+	if err := foundRestaurants.All(context, &restaurants); err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	return http.StatusOK, restaurants
 }
-func (r *MongoUsersRepository) GetRestaurantById(context *gin.Context) *models.User {
-	return &models.User{}
+func (r *MongoUsersRepository) GetRestaurantById(context *gin.Context) (statusCode int, response interface{}) {
+
+	var restaurant models.User
+
+	// De el filtro aplicado el que no funciona es el de id
+	filter := bson.M{"role": "restaurant", "id": context.Param("id")}
+	if err := r.users.FindOne(context, filter).Decode(&restaurant); err != nil {
+		return http.StatusNotFound, err.Error()
+	}
+
+	return http.StatusOK, restaurant
 }
-func (r *MongoUsersRepository) FindClients(context *gin.Context) *[]models.User {
-	return &[]models.User{}
+
+func (r *MongoUsersRepository) FindClients(context *gin.Context) (statusCode int, response interface{}) {
+
+	foundRestaurants, err := r.users.Find(context, bson.M{"role": "client"})
+	if err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	clients := []*models.User{}
+	if err := foundRestaurants.All(context, &clients); err != nil {
+		return http.StatusConflict, err.Error()
+	}
+
+	return http.StatusOK, clients
 }
-func (r *MongoUsersRepository) GetClientById(context *gin.Context) *models.User {
-	return &models.User{}
+
+func (r *MongoUsersRepository) GetClientById(context *gin.Context) (statusCode int, response interface{}) {
+	var client models.User
+
+	// De el filtro aplicado el que no funciona es el de id
+	filter := bson.M{"role": "client", "id": context.Param("id")}
+	if err := r.users.FindOne(context, filter).Decode(&client); err != nil {
+		return http.StatusNotFound, err.Error()
+	}
+
+	return http.StatusOK, &client
 }
-func (r *MongoUsersRepository) GetRestaurantProducts(context *gin.Context) *[]models.Product {
-	return &[]models.Product{}
+
+// Hasta que no funcione el buscar un elemento por id poco podemos hacer con estos endpoints
+func (r *MongoUsersRepository) GetRestaurantProducts(context *gin.Context) (statusCode int, response interface{}) {
+	return 0, &[]models.Product{}
 }
-func (r *MongoUsersRepository) UpdateProfile(context *gin.Context) *models.User {
-	return &models.User{}
+func (r *MongoUsersRepository) UpdateProfile(context *gin.Context) (statusCode int, response interface{}) {
+	return 0, &models.User{}
 }
 
 // restaurant_role methods
-func (r *MongoUsersRepository) FindProducts(context *gin.Context) *[]models.Product {
-	return &[]models.Product{}
+// Para estos necesitamos comprobar que el restaurante tiene permisos y sacar su id de la sesion para poder efectuar las acciones
+func (r *MongoUsersRepository) FindProducts(context *gin.Context) (statusCode int, response interface{}) {
+	return 0, &[]models.Product{}
 }
-func (r *MongoUsersRepository) CreateProduct(context *gin.Context) *models.Product {
-	return &models.Product{}
+func (r *MongoUsersRepository) CreateProduct(context *gin.Context) (statusCode int, response interface{}) {
+	return 0, &models.Product{}
 }
-func (r *MongoUsersRepository) UpdateProduct(context *gin.Context) *models.Product {
-	return &models.Product{}
+func (r *MongoUsersRepository) UpdateProduct(context *gin.Context) (statusCode int, response interface{}) {
+	return 0, &models.Product{}
 }
