217ea6051ca691389d98edcea9428aa1d8d6cbee
Nestor Torres <alu0101021768@ull.edu.es>
2021-12-05 01:34:03 +0000

    models: minor changes in order model and adds some comments

--- src/models/order.go
+++ src/models/order.go
@@ -8,8 +8,8 @@ import (
 
 type Order struct {
 	Id           primitive.ObjectID `json:"id" bson:"id"`
-	RestaurantId primitive.ObjectID `json:"restaurantId" bson:"restaurantId" validate:"required"`
-	ClientId     primitive.ObjectID `json:"clientId" bson:"clientId" validate:"required"`
+	RestaurantId primitive.ObjectID `json:"restaurantId" bson:"restaurantId"`
+	ClientId     primitive.ObjectID `json:"clientId" bson:"clientId" validate:"required"` // cuando tengamos JWT quitaremos el validate de aqui
 	Status       enums.OrderStatus  `json:"status" bson:"status"  validate:"required"`
 	TotalPrice   float64            `json:"totalPrice" bson:"totalPrice" validate:"required"`
 	Products     []ProductInfo      `json:"products,omitempty" bson:"products,omitempty" validate:"required,dive,required"`
