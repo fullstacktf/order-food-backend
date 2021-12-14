0ac8476751e6d2ee9fed8fa56b354a2a858b92e5
Nestor Torres <alu0101021768@ull.edu.es>
2021-12-14 19:16:35 +0000

    adding cors

--- src/app.go
+++ src/app.go
@@ -27,13 +27,13 @@ func main() {
 
 	r := gin.Default()
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowOrigins:     []string{"http://localhost:8080"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		AllowOriginFunc: func(origin string) bool {
-			return origin == "http://localhost:3000"
+			return origin == "http://localhost:8080"
 		},
 		MaxAge: 12 * time.Hour,
 	}))
