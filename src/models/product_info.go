bc072a3d6ed838e2a496ba0fa5d7907f29c551aa
Nestor Torres <alu0101021768@ull.edu.es>
2021-12-21 14:30:25 +0000

    chore: adding name field to products info model

--- src/models/product_info.go
+++ src/models/product_info.go
@@ -4,6 +4,7 @@ import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type ProductInfo struct {
 	ProductId primitive.ObjectID `json:"productId" bson:"productId"`
+	Name      string             `json:"name" bson:"name" validate:"required"`
 	Quantity  int                `json:"quantity" bson:"quantity" validate:"required"`
 	Price     float64            `json:"price" bson:"price" validate:"required"`
 }
